01c3dfeb8ba54dad53370bed8469d05220691d65
Proper <properprogress4@gmail.com>
2024-09-13 14:48:34 +0100

    Event management contract

---
+++ contracts/EventTicket.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+interface IEVNFTContract {
+    function balanceOf(address owner) external view returns (uint256);
+}
+
+contract EventManagement {
+
+    uint256 public eventCount;
+    mapping(uint256 => Event) public events;
+    mapping(address => bool) public isNFTOwner;
+    address public nftContractAddress;
+
+    struct Event {
+        uint256 id;
+        string name;
+        string description;
+        uint256 timestamp;
+        address creator;
+        bool canceled;
+    }
+
+    constructor(address _nftContractAddress) {
+        nftContractAddress = _nftContractAddress;
+    }
+
+    function createEvent(string memory _name, string memory _description) public {
+        require(isNFTOwner[msg.sender], "Only NFT owners can create events");
+        require(eventCount < 10, "Maximum event limit reached");
+
+        eventCount++;
+        events[eventCount] = Event(eventCount, _name, _description, block.timestamp, msg.sender, false);
+    }
+
+    function checkNFTOwnership() public {
+        require(nftContractAddress != address(0), "NFT contract address not set");
+
+        uint256 balance = IEVNFTContract(nftContractAddress).balanceOf(msg.sender);
+        isNFTOwner[msg.sender] = balance > 0;
+    }
+
+    function updateEvent(uint256 _eventId, string memory _name, string memory _description) public {
+        require(isNFTOwner[msg.sender], "Only event creators can update event");
+        require(events[_eventId].creator == msg.sender, "Only event creator can update");
+
+        events[_eventId].name = _name;
+        events[_eventId].description = _description;
+    }
+
+    function cancelEvent(uint256 _eventId) public {
+        require(isNFTOwner[msg.sender], "Only event creators can cancel event");
+        require(events[_eventId].creator == msg.sender, "Only event creator can cancel");
+
+        events[_eventId].canceled = true;
+    }
+}
+
+
