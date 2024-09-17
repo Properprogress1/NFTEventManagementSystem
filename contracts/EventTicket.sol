18227387c24f3e7a755621ef42fe2fde5706a61d
Proper <properprogress4@gmail.com>
2024-09-17 02:57:26 +0100

    json files

--- contracts/EventTicket.sol
+++ contracts/EventTicket.sol
@@ -5,12 +5,12 @@ interface IEVNFTContract {
     function balanceOf(address owner) external view returns (uint256);
 }
 
-contract EventManagement {
+contract EventTicket {
     address public nftContractAddress;
     uint256 public eventCount;
+
     mapping(uint256 => Event) public events;
     mapping(address => bool) public isNFTOwner;
-   
 
     struct Event {
         uint256 id;
@@ -33,7 +33,13 @@ contract EventManagement {
         events[eventCount] = Event(eventCount, _name, _description, block.timestamp, msg.sender, false);
     }
 
-    // function 
+    function registerEvent(uint256 _eventId) public view {
+    require(isNFTOwner[msg.sender], "Only NFT owners can register for event");
+    require(!events[_eventId].canceled, "Event is canceled");
+
+    // Add logic to register the user for the event, e.g., store the user's address in a mapping or list.
+}
+
 
     function checkNFTOwnership() public {
         require(nftContractAddress != address(0), "NFT contract address not set");
@@ -41,6 +47,7 @@ contract EventManagement {
         uint256 balance = IEVNFTContract(nftContractAddress).balanceOf(msg.sender);
         isNFTOwner[msg.sender] = balance > 0;
     }
+
 
     function updateEvent(uint256 _eventId, string memory _name, string memory _description) public {
         require(isNFTOwner[msg.sender], "Only event creators can update event");
@@ -60,3 +67,11 @@ contract EventManagement {
 }
 
 
+
+
+// function registerEvent(uint256 _eventId) public {
+//     require(isNFTOwner[msg.sender], "Only NFT owners can register for event");
+//     require(!events[_eventId].canceled, "Event is canceled");
+
+//     // Add logic to register the user for the event, e.g., store the user's address in a mapping or list.
+// }
